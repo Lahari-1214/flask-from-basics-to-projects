d5400b031a5b90a99b4aa81f13f9ec99f84e6ae4
Leela <leelanarayan1214@gmail.com>
2026-02-27 10:26:12 +0530

    Creating new data through POST request

--- 004_Request_Handling/README.md
+++ 004_Request_Handling/README.md
@@ -9,3 +9,13 @@ Parameters appear in the URL → not secure for sensitive data.
 note: Data comes from query parameters using request.args.get()
 
 
+### POST Request
+##### Definition:
+The POST method is used to send data securely to the server (e.g., form data).
+Data is sent in the body of the request, not the URL.
+Commonly used for form submissions or data insertion.
+##### Usage:
+Used for creating new data or sending confidential data.
+Not visible in the URL → more secure.
+
+
--- 004_Request_Handling/app.py
+++ 004_Request_Handling/app.py
@@ -4,16 +4,36 @@ app = Flask(__name__)
 def Home():
     return "Welcome Home"
 
-# Get request
+# Get request # url:http://127.0.0.1:5000/get_data?name=Jani&age=24 
 @app.route('/getdata',methods=['GET'])
 def GetData():
     name = request.args.get('name')
     return f"How can I help you!{name}"
 
+# POST Request
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/postdata', methods=['GET', 'POST'])
+def postdata():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        return f"Hello {username} and your registered email is {email}"
+    else:
+        return """
+            <form action='/postdata' method='post'>
+                <input type='text' name='username' placeholder='Enter username' required>
+                <input type='email' name='email' placeholder='Enter email' required>
+                <button type='submit'>Submit</button>
+            </form>
+        """
+
 if __name__ == '__main__':
     app.run(debug=True)
     
-# url:http://127.0.0.1:5000/get_data?name=Jani&age=24  
+ 
 
 
-# POST Request
+
