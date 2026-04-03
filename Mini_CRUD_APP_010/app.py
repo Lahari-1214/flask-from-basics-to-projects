3d6e8f4451ee4d82218aeccd075497bc41abca2a
Leela <leelanarayan1214@gmail.com>
2026-04-03 23:55:59 +0530

    Creating Data into DB using forms in HTML

---
+++ Mini_CRUD_APP_010/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect
+import mysql.connector
+app = Flask(__name__)
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "your_password",
+    database = "your DB name"
+
+)
+@app.route('/')
+def register_page():
+    return render_template("register.html")
+
+
+@app.route('/register',methods = ['POST'])
+def Register():
+    fname = request.form['firstname']
+    lname = request.form['lastname']
+    email = request.form['email']
+    uname = request.form['username']
+    pwd = request.form['password']
+    cpwd = request.form['cpassword']
+
+    if pwd != cpwd:
+        return "Confirm the password"
+    
+    query = "INSERT INTO users(firstname, lastname, email, username, password) VALUES (%s,%s,%s,%s,%s)"
+    cursor = mydb.cursor()
+    cursor.execute(query, (fname, lname, email, uname, pwd,))
+    mydb.commit()
+    return " Registration Successful! <br><a href='/login'>Go to Login</a>"
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
