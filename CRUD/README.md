a6842e57f26d760cc6f8829a44ffba20977a72f0
Leela <leelanarayan1214@gmail.com>
2026-04-03 22:15:30 +0530

    Testing DB connection with flask

---
+++ CRUD/README.md
@@ -0,0 +1,6 @@
+1. Create virtual Environment using python -m venv venv
+2. Activate the venv using venv\Scripts\activate
+3. install flask --> pip install flask
+4. install mysql --> pip install mysql-connector-python
+5. create a connection using db object with 4 parameters
+6. Test the connection with dummy route.
---
+++ CRUD/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request,jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+mydb = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = 'yourpassword',
+    database = 'yourdatabase'
+)
+
+@app.route('/')
+def Home():
+    return "DB connection is Success...... "
+
+@app.route('/getusers',methods=['GET'])
+def Getallusers():
+    cursor = mydb.cursor()
+    cursor.execute("select * from users")
+    result = cursor.fetchall()
+    cursor.close()
+    return jsonify(result)
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
